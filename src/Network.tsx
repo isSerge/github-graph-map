0ad0b638151119801ab4138d3ab64ce21780a466
Serge Kovbasiuk <isserge@protonmail.com>
2025-02-10 15:13:27 +0700

    use custom node component

--- src/Network.tsx
+++ src/Network.tsx
@@ -1,4 +1,4 @@
-import { ResponsiveNetwork, NetworkDataProps, NodeTooltipProps } from '@nivo/network'
+import { ResponsiveNetwork, NetworkDataProps, NodeProps } from '@nivo/network'
 import { NetworkLink, RepoNode, ContributorNode } from './types'
 
 const theme = {
@@ -16,26 +16,32 @@ function isRepoNode(node: Node): node is RepoNode {
   return (node as RepoNode).stargazerCount !== undefined;
 }
 
-const Tooltip = ({ node }: NodeTooltipProps<Node>) => (
-  <div
-      style={{
-          background: node.color,
-          color: '#000000',
-          padding: '9px 12px',
-          borderRadius: '2px',
-          boxShadow: '0 3px 9px rgba(0, 0, 0, .35)',
-      }}
-  >
-      <strong>{node.id}</strong>
-      <br />
-      {isRepoNode(node.data) && (
-          <span>
-              {node.data.stargazerCount} stargazers
-              <br />
-          </span>
-      )}
-  </div>
-)
+const NodeComponent = ({ node, onClick }:  NodeProps<Node>) => {
+  const style = {
+    cursor: 'pointer',
+  }
+
+  const repoNode = (
+    <g transform={`translate(${node.x},${node.y})`} style={style} onClick={onClick ? event => onClick(node, event) : undefined}>
+      <circle r={10} fill={node.color} />
+      <text y="20" textAnchor="middle" fontSize="12" fill="#000000">
+            {node.id}
+        </text>
+    </g>
+  )
+
+  const contributorNode = (
+    <g transform={`translate(${node.x - 12},${node.y - 18})`} style={style} onClick={onClick ? event => onClick(node, event) : undefined}>
+        <circle cx="12" cy="8" r="5" fill={node.color} stroke="#ffffff" />
+        <path d="M3,21 h18 C 21,12 3,12 3,21" fill={node.color} stroke="#ffffff" />
+        <text x="12" y="32" textAnchor="middle" fontSize="12" fill="#000000">
+            {node.id}
+        </text>
+    </g>
+);
+  
+  return isRepoNode(node.data) ? repoNode : contributorNode;
+}
 
 const Network = ({ data }: NetworkDataProps<Node, NetworkLink>) => (
     <ResponsiveNetwork
@@ -45,9 +51,10 @@ const Network = ({ data }: NetworkDataProps<Node, NetworkLink>) => (
         linkDistance={e=>e.distance}
         nodeColor={e=>e.color}
         linkColor="rgb(255, 230, 0)"
-        repulsivity={200}
+        repulsivity={300}
         centeringStrength={0.5}
-        nodeTooltip={Tooltip}
+        nodeComponent={NodeComponent}
+        onClick={(node) => console.log(node)}
     />
 )
 

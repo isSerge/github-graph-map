102b24b711a37d2c564d4f1b1a8153ae636d39aa
Serge Kovbasiuk <isserge@protonmail.com>
2025-02-14 06:50:31 +0700

    refactor types

--- src/types.ts
+++ src/types.ts
@@ -1,70 +1,62 @@
-import { InputNode, InputLink } from '@nivo/network'
+import { InputNode, InputLink } from '@nivo/network';
 
+// Base type for repository data as returned by the API.
+export interface RepoBase {
+  name: string;
+  stargazerCount: number;
+  description: string;
+  primaryLanguage: {
+    name: string;
+  };
+  url: string;
+  owner: {
+    login: string;
+  };
+  pushedAt: string;
+}
+
+// Common network node interface.
 export interface NetworkNode extends InputNode {
-    name: string
-    type: "repo" | "contributor"
+  name: string;
+  type: "repo" | "contributor";
 }
 
-export interface RepoNode extends NetworkNode {
-    name: string
-    stargazerCount: number
-    description: string
-    primaryLanguage: {
-        name: string
-    }
-    url: string
-    owner: {
-        login: string
-    }
-    pushedAt: string
-}
+// RepoNode is a combination of the repository data and network node info.
+export type RepoNode = RepoBase & NetworkNode & { type: "repo" };
 
 export interface ContributorNode extends NetworkNode {
-    avatarUrl: string;
-    company: string;
-    email: string;
-    followers: {
-      totalCount: number;
-    };
-    following: {
-      totalCount: number;
-    };
-    location: string;
-    login: string;
-    organizations: {
-      nodes: {
-        login: string;
-      }[];
-    };
-    websiteUrl: string;
-    topRepositories: {
-      totalCount: number;
-      nodes: RepoData[];
-    }
-    repositoriesContributedTo: {
-      totalCount: number;
-      nodes: RepoData[];
-    };
+  avatarUrl: string;
+  company: string;
+  email: string;
+  followers: {
+    totalCount: number;
+  };
+  following: {
+    totalCount: number;
+  };
+  location: string;
+  login: string;
+  organizations: {
+    nodes: {
+      login: string;
+    }[];
+  };
+  websiteUrl: string;
+  topRepositories: {
+    totalCount: number;
+    nodes: RepoBase[];
+  };
+  repositoriesContributedTo: {
+    totalCount: number;
+    nodes: RepoBase[];
+  };
+  type: "contributor";
 }
 
 export type EitherNode = RepoNode | ContributorNode;
 
 export interface NetworkLink extends InputLink {
-    source: string
-    target: string
-    distance: number
-}
-
-export interface RepoData {
-    name: string
-    stargazerCount: number
-    description: string
-    primaryLanguage: {
-        name: string
-    }
-    url: string
-    owner: {
-        login: string
-    }
-    pushedAt: string
+  source: string;
+  target: string;
+  distance: number;
 }

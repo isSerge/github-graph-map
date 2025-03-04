4cc2f88cb14048baae9c8bdf92b3a1b5824b56ae
Serge Kovbasiuk <isserge@protonmail.com>
2025-03-04 07:16:58 +0100

    add query client persistence with local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
+
 import AppRouter from "./AppRouter";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 2 * 60 * 60 * 1000,
+    },
+  },
+});
+
+const localStoragePersistor = createWebStoragePersistor({ storage: window.localStorage });
+
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
